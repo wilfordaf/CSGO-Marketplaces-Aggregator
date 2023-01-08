3aca11f521923cd60151c573c0ee171719bf45de
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2023-01-08 03:36:44 +0300

    feat: simple test logic

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import database_contollers.DatabaseItems as items_db
-import database_contollers.DatabaseUserTrackedItems as user_tracked_items_db
+import time
 
+from database_interaction.DatabaseController import DatabaseController
 from parcers.LootFarmParser import LootFarmParser
+from notification_providers.TelegramNotificationProvider import TelegramNotificationProvider
+from notification_providers.NotificationTextFormatter import NotificationTextFormatter
 
 
 def main():
-    item = "M9 Bayonet | Tiger Tooth"
     lootfarm_parser = LootFarmParser()
-    result = lootfarm_parser.parse_response(lootfarm_parser.request_market(), item)
-    print(*result, sep="\n")
+    database_controller = DatabaseController()
+    telegram_notification_provider = TelegramNotificationProvider()
+    item = "Glock-18"
+    user_id = "123456789"
+
+    while True:
+        result = lootfarm_parser.parse_response(lootfarm_parser.request_market(), item)
+        new_items, sold_items = database_controller.get_item_difference(user_id, result)
+        database_controller.update_user(user_id, result)
+        text = NotificationTextFormatter.format_sold_new_message(new_items, sold_items)
+        telegram_notification_provider.send_notification(text)
+        time.sleep(20)
 
 
 if __name__ == "__main__":
